6dd8339b6a02d66e2efbc3b9bc5d6702f9032787
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2012-05-03 10:06:04 +0000

    pin minimal version requirements

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup, find_packages
 version = '1.1.dev0'
 
 install_requires = [
-    'setuptools',
+    'Chameleon >= 2.8.4',
     'grokcore.component',
     'grokcore.view',
-    'Chameleon >= 2.0-rc1',
-    'z3c.pt >= 2.0-rc1',
+    'setuptools',
+    'z3c.pt >= 2.2.2',
     'zope.i18n',
     ]
 
@@ -18,37 +18,40 @@ tests_require = [
     'zope.app.appsetup',
     ]
 
-long_description = (open('README.txt').read() +
-                    '\n\n' +
-                    open(os.path.join('src', 'grokcore', 'chameleon',
-                                      'README.txt')).read() +
-                    '\n\n' +
-                    open('CHANGES.txt').read())
+long_description = (
+    open('README.txt').read() +
+    '\n\n' +
+    open(os.path.join('src', 'grokcore', 'chameleon',
+    'README.txt')).read() +
+    '\n\n' +
+    open('CHANGES.txt').read())
 
-setup(name='grokcore.chameleon',
-      version=version,
-      description="Chameleon page template support for Grok",
-      long_description=long_description,
-      # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=['Development Status :: 3 - Alpha',
-                   'Environment :: Web Environment',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: Zope Public License',
-                   'Programming Language :: Python :: 2.5',
-                   'Operating System :: OS Independent',
-                   'Topic :: Internet :: WWW/HTTP',
-                   ],
-      keywords="grok chameleon template",
-      author="Uli Fouquet",
-      author_email="grok-dev@zope.org",
-      url="http://pypi.python.org/pypi/grokcore.chameleon",
-      license="ZPL",
-      package_dir={'': 'src'},
-      packages=find_packages('src'),
-      namespace_packages=['grokcore'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=install_requires,
-      tests_require=tests_require,
-      extras_require={'test': tests_require},
-      )
+setup(
+    name='grokcore.chameleon',
+    version=version,
+    description="Chameleon page template support for Grok",
+    long_description=long_description,
+    # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python :: 2.5',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        ],
+    keywords="grok chameleon template",
+    author="Uli Fouquet",
+    author_email="grok-dev@zope.org",
+    url="http://pypi.python.org/pypi/grokcore.chameleon",
+    license="ZPL",
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    namespace_packages=['grokcore'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+    )
